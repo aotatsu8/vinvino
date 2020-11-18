5e0dc08160a082b59c5f174e4ce4e2758b1a83b1
EC2 Default User <ec2-user@ip-172-31-44-198.ap-northeast-1.compute.internal>
2020-11-18 12:05:08 +0000

    細かな修正profile昨日追加

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -50,11 +50,12 @@
           製作者について
         </a>
       </li>
-    </ul>
-    //<form class="form-inline my-2 my-lg-0">
-      //<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      //<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </ul>{{--
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+    --}}
   </div>
      </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
